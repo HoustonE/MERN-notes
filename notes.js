9759545875fd19189efd26a919817bc0f828ef64
Ethan Houston <48807099+HoustonE@users.noreply.github.com>
2021-01-06 22:16:52 -0500

    figured out post method with axios for adding notes. Unsure if useEffect is necessary for full API handling, probably just for mounting

---
+++ notes.js
@@ -0,0 +1,22 @@
+// JS objects for testing
+
+module.exports = [
+    {
+      id: 1,
+      title: "Ducks",
+      content:
+        "they go quack"
+    },
+    {
+      id: 2,
+      title: "Loops",
+      content:
+        "of the fruit variety and other sugary cereals"
+    },
+    {
+      id: 3,
+      title: "mantArrays",
+      content:
+        "sorted, and stingy"
+    },
+  ];
--- server.js
+++ server.js
@@ -5,15 +5,30 @@ const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const notes = require('./notes');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(bodyParser.json());
+
 app.get('/ping', function (req, resp) {
- return resp.send('pong');
+  return resp.send('pong');
 });
 
 app.get('/', function (req, resp) {
   resp.send({express : 'request sent back from Express'});
+});
+
+app.get('/notes', function (req, resp){
+  resp.send(notes);
+});
+
+app.post('/notes', function(req, resp){
+  //todo add received note in todo -- redirect to return get /notes
+  console.log("POST notes call received");
+  console.log(req.body.title);
+  console.log(req.body);
+  resp.send("HELLO FROM POST METHOD");
 });
 
 let port = process.env.PORT;
